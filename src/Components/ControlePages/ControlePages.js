aeee4b972df3329d15fb72eb37209d4ae844c5bb
Shamil Samusenko <Shamilllaa@mail.ru>
2021-02-25 12:50:19 +0700

    v2

--- src/Components/ControlePages/ControlePages.js
+++ src/Components/ControlePages/ControlePages.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useStyles } from './controlePagesStyle';
 
@@ -6,14 +6,19 @@ export const ControlePages = ({ btn, plusBtn, minusBtn, required }) => {
 	const classes = useStyles();
 	const price = useSelector(state => state.app.price);
 	const selectData = useSelector(state => state.app.selectData);
-	useEffect(() => {
-		let text = document.querySelector(`.${classes.controlePages__text}`)
-		if (!btn) text.innerHTML = `Не могу найти нужные исследования`
-		else if (btn) text.innerHTML = `Нажимая на кнопку, вы принимаете 
-			<span>Оферту об оказании услуг</span> и даете согласие на обработку 
-			персональных данных в соответствии с <span>Политикой конфиденциальности</span>`
-	// eslint-disable-next-line
-	}, [btn])
+
+	const effect = () => {
+		if (!btn) {
+			return <p className={classes.controlePages__text}>Не могу найти нужные исследования</p>
+		}
+		else {
+			return 	(
+				<p className={classes.controlePages__text}>
+					Нажимая на кнопку, вы принимаете <span>Оферту об оказании услуг</span> и даете согласие на обработку 
+					персональных данных в соответствии с <span>Политикой конфиденциальности</span>
+				</p>)
+		}
+	}
 
     return (
         <div className={classes.controlePages}>
@@ -38,7 +43,7 @@ export const ControlePages = ({ btn, plusBtn, minusBtn, required }) => {
 					</span>
                 </button>
             </div>
-            <p className={classes.controlePages__text}></p>
+			{effect()}
         </div>
     )
 }
